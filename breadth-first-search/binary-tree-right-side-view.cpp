705bd789d2cddbb5bbbc4b5e1769c0cb278ed319
Gokul K <k.gokul27112007@gmail.com>
2026-09-04 20:29:36 +0530

    LC-binary-tree-right-side-view [Runtime: N/A; Memory: N/A]

---
+++ breadth-first-search/binary-tree-right-side-view.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> rightSideView(TreeNode* root) {
+        if(!root) return{};
+        queue<TreeNode*> q;
+        vector<int> ans;
+        q.push(root);
+        while(!q.empty())
+        {   
+            int n=q.size();
+            vector<int> res;
+            for(int i=0;i<n;i++)
+            {
+                TreeNode* temp=q.front();
+                q.pop();
+                res.push_back(temp->val);
+                if(temp->left) q.push(temp->left);
+                if(temp->right) q.push(temp->right);
+            }
+            ans.push_back(res.back());
+        }
+        return ans;
+    }
+};
