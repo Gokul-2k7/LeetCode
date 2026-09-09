c6fa59ab19076a5798427b126a826ad7605097de
Gokul K <k.gokul27112007@gmail.com>
2026-09-09 19:55:51 +0530

    LC-minimum-window-substring [Runtime: N/A; Memory: N/A]

--- string/minimum-window-substring.cpp
+++ string/minimum-window-substring.cpp
@@ -2,20 +2,21 @@ class Solution {
 public:
     string minWindow(string s, string t) {
         unordered_map<char,int> d1;
-        unordered_map<char,int> d2;int flag=1;
+        unordered_map<char,int> d2;int formed=0;
         for(int i=0;i<t.size();i++)
         {
             d2[t[i]]+=1;
         }
+        int req=t.size();
         int i=0,j=0;int a=0,b=0,minval=INT_MAX;
         for(;j<s.size();j++)
         {
             if( d2.contains(s[j]) && d2[s[j]]>=1)
             {
                 d1[s[j]]+=1;
-                if(d1[s[j]]>=d2[s[j]]) flag=1;
-                else flag=0;
-                while(d1.size()==d2.size() && flag)
+                if(d1[s[j]]<=d2[s[j]]) formed++;
+                
+                while(formed==req)
                 {   
                     if(minval>j-i+1)
                     {
@@ -24,7 +25,9 @@ public:
                         b=j+1;
                     }
                     if(d1.contains(s[i]))
-                    {   d1[s[i]]-=1;
+                    {   
+                        if(d1[s[i]]<=d2[s[j]]) formed--;
+                        d1[s[i]]-=1;
                          if(d1[s[i]]==0) d1.erase(s[i]);
                     }
                     i++;
