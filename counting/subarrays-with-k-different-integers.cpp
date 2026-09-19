7e2c2c969324edf179acbd139f4a3b4cd48b4ddc
Gokul K <k.gokul27112007@gmail.com>
2026-09-19 10:55:24 +0530

    LC-subarrays-with-k-different-integers [Runtime: N/A; Memory: N/A]

---
+++ counting/subarrays-with-k-different-integers.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int subarraysWithKDistinct(vector<int>& nums, int k) {
+        int i=0;int j=0;
+        unordered_map<int,int> d;
+        int count=0;
+        while(j<nums.size() && i<=j)
+        {
+            d[nums[j]]+=1;
+           
+            while(d.size()>k)
+            {
+                d[nums[i]]--;
+                if(d[nums[i]]==0) d.erase(nums[i]);
+                i++;
+            }
+            count+=j-i+1;
+            j++;
+        }
+        i=0;j=0;int count2=0;
+        d.clear();
+        k--;
+        while(j<nums.size() && i<=j)
+        {   if(k<0) break;
+            d[nums[j]]+=1;
+           
+            while(d.size()>k)
+            {
+                d[nums[i]]--;
+                if(d[nums[i]]==0) d.erase(nums[i]);
+                i++;
+            }
+            count2+=j-i+1;
+            j++;
+        }
+        return count-count2;
+    }
+};
